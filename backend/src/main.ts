b7b4259a12dad6f6e57adf96ac3cccea31442fd3
Dmitrii <ffurzy@gmail.com>
2026-02-16 15:05:09 -0500

    FIX: DTO

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,18 @@ import { webcrypto } from 'node:crypto';
 globalThis.crypto ??= webcrypto as any;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
   await app.listen(Number(process.env.PORT) || 3000);
