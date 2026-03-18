54fc0acb04562ce874c911076f73f78560a35743
Dmitrii <ffurzy@gmail.com>
2026-03-18 18:05:37 -0400

    feat: add and conected loggers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ globalThis.crypto ??= webcrypto as any;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DevLogger } from './logger/dev.logger';
+import { JsonLogger } from './logger/json.logger';
+import { TskvLogger } from './logger/tskv.logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +19,9 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
+  app.useLogger(new TskvLogger());
+  app.useLogger(new DevLogger());
+  app.useLogger(new JsonLogger());
   await app.listen(Number(process.env.PORT) || 3000);
 }
 bootstrap();
